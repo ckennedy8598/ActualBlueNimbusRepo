9f9e5145b96ee11e1d3f08e072e3efb357b99444
Jahpacho <158857348+Jahpacho@users.noreply.github.com>
2024-04-25 22:35:20 -0400

    Small Boss Axe changes
    The boss axe isn't done yet FYI. Please let this work

---
+++ ActualBlueNimbusProject/Assets/Boss_Axe_Hitbox.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Axe_Hitbox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public PlayerCombat playerHealth;
+    [SerializeField] public int damage = 2;
+
+    void Start()
+    {
+        playerHealth = FindObjectOfType<PlayerCombat>();
+        if (playerHealth == null)
+        {
+            Debug.LogError("PlayerCombat component not found in the scene.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerHealth.TakeDamage(damage);
+            Debug.Log("Player Taking Damage");
+        }
+    }
+}
