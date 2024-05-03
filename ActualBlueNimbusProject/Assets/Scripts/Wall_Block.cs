d3d5189e7c8fd30ba2cbe888d2b66efe498659dd
combine1201 <steamuser27@gmail.com>
2024-05-03 15:16:41 -0400

    Made Left Blocking Wall Wall-Jumpable
    Implemented
     - Wall-Jumpable Wall
     - Additional Wall to Keep Players in (Not Jumpable)

--- ActualBlueNimbusProject/Assets/Scripts/Wall_Block.cs
+++ ActualBlueNimbusProject/Assets/Scripts/Wall_Block.cs
@@ -6,11 +6,13 @@ public class Wall_Block : MonoBehaviour
 {
     [SerializeField] public GameObject BlockingWall;
     [SerializeField] public GameObject BlockingWall3;
+    [SerializeField] public GameObject BlockingWall4;
     private bool wallActive = false;
     // Start is called before the first frame update
     void Start()
     {
         BlockingWall.SetActive(false);
+        BlockingWall4.SetActive(false);
         BlockingWall3.SetActive(true);
     }
 
@@ -26,6 +28,7 @@ public class Wall_Block : MonoBehaviour
         {
             Debug.Log("Player Passed Wall Point");
             BlockingWall.SetActive(true);
+            BlockingWall4.SetActive(true);
             BlockingWall3.SetActive(false);
             wallActive = true;
         }
